eedfd2699f3460260f66fcd4598ddffca2825a69
Nathanael Lu <lunathanael@gmail.com>
2024-01-27 14:46:23 -0500

    testing google maps api

---
+++ Frontend/screens/MapTest.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { ImageBackground, StyleSheet, StatusBar, Dimensions } from 'react-native';
+import { Block, Button, Text, theme } from 'galio-framework';
+
+const { height, width } = Dimensions.get('screen');
+
+import materialTheme from '../constants/Theme';
+import Images from '../constants/Images';
+
+import Test from './test.js'
+
+export default class Onboarding extends React.Component {
+  render() {
+    const { navigation } = this.props;
+
+    return (
+      <Block flex style={styles.container}>
+        <Test/>
+      </Block>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "black",
+  },
+  padded: {
+    paddingHorizontal: theme.SIZES.BASE * 2,
+    position: 'relative',
+    bottom: theme.SIZES.BASE,
+  },
+  button: {
+    width: width - theme.SIZES.BASE * 4,
+    height: theme.SIZES.BASE * 3,
+    shadowRadius: 0,
+    shadowOpacity: 0,
+  },
+});
