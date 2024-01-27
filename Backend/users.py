594b964c56e2e685d5195850a8d786839efee7a6
Aidan Keighron <78337317+aidankeighron@users.noreply.github.com>
2024-01-27 11:31:41 -0500

    getting data

--- Backend/users.py
+++ Backend/users.py
@@ -1,6 +1,32 @@
-from firebase_connection import FirebaseConnection
+from __future__ import annotations
+from firebase_connection import FirebaseConnection, User, DiningHall, Class, Menu, Food, Location
+import numpy as np, math
+from numpy import ndarray
 
-firebase_connection = FirebaseConnection()
+MAX_WALKING_DISTANCE = 1.5 # miles
+TIME_TO_EAT = 30 # minutes
 
-def plan_route(user: dict) -> list:
-    user_data = firebase_connection.get_user_data(user)
+firebase_connection: FirebaseConnection = FirebaseConnection()
+dining_halls: list[DiningHall] = firebase_connection.get_dining_halls()
+
+def plan_route(user: str) -> list:
+    user: User = firebase_connection.get_user_data(user)
+    
+    week: list[list[Class]] = [[],[],[],[],[],[],[]]
+    for class_data in user.classes:
+        for day in class_data.days:
+            week[day].append(class_data)
+            
+    week = [sorted(day, key=lambda class_data: class_data.start) for day in week]
+        
+    for day in week:
+        if not day:
+            continue
+        possible_lunch_times = []
+        
+        for dining_hall in dining_halls:
+            if dining_hall.location.get_distance(user.location) > MAX_WALKING_DISTANCE:
+                continue
+            
+
+plan_route("Aidan")
