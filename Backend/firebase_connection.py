3442a8067262e32a3f7d185961c3334e98c824fa
Aidan Keighron <78337317+aidankeighron@users.noreply.github.com>
2024-01-27 14:46:17 -0500

    get food info

--- Backend/firebase_connection.py
+++ Backend/firebase_connection.py
@@ -1,11 +1,15 @@
 from __future__ import annotations
-import firebase_admin
+from bs4 import BeautifulSoup
 from firebase_admin import db
-import os, math
+from datetime import date
+import os, math, urllib.request, firebase_admin
 
+DINING_HALLS_TO_HTML = {'South Pointe at Case': 'South%20Pointe%20at%20Case', 'Sparty\'s Market': 'Sparty%27s%20market', 'The Edge at Akers': 'The%20Edge%20at%20Akers', 'Brody Square': 'Brody%20Square', 'Holden Dining Hall': 'Holden%20Dining%20Hall', 'Holmes Dining Hall': 'Holmes%20Dining%20Hall', 'The State Room at Kellogg': 'The%20State%20Room%20at%20Kellogg', 'Heritage Commons at Landon': 'Heritage%20Commons%20at%20Landon', 'Thrive at Owen': 'Thrive%20at%20Owen', 'The Vista at Shaw': 'The%20Vista%20at%20Shaw', 'The Gallery at Snyder Phillips': 'The%20Gallery%20at%20Snyder%20Phillips'}
+MEAL_TIMES = ['breakfast', 'lunch', 'dinner']
 RESET_USERS = False
-RESET_DINING_HALLS = False
+RESET_DINING_HALLS = True
 STAY_IN_DINING_HALL_AFTER_CLOSE = False
+MINUTES_PER_MILE = 15
 
 class Location:
     def __init__(self, lat: float, long: float):
@@ -19,17 +23,17 @@ class Location:
     def __lat_long_to_miles(self, degrees: float, lat: bool) -> float:
         return degrees * (69 if lat else 54.6)
 
-    def get_distance(self, other: Location) -> float:
+    def get_distance_miles(self, other: Location) -> float:
         return math.hypot(self.__lat_long_to_miles(self.lat, True)-self.__lat_long_to_miles(other.lat, True),
                           self.__lat_long_to_miles(self.long, False)-self.__lat_long_to_miles(other.long, False))
         
-    def get_time_to_walk(self, other: Location) -> int:
+    def get_time_to_walk_hours(self, other: Location) -> int:
         # TODO use google maps api to get time to walk
-        return self.get_path_distance(other) * 15 # 15 minutes per mile
+        return self.get_path_distance_miles(other) * MINUTES_PER_MILE # 15 minutes per mile
         
-    def get_path_distance(self, other: Location) -> float:
+    def get_path_distance_miles(self, other: Location) -> float:
         # TODO use google maps api to get distance
-        return self.get_distance(other)
+        return self.get_distance_miles(other)
 
 class Class:
     def __init__(self, name: str, location: dict[str, int], time: dict[str, int], days: list):
@@ -46,15 +50,37 @@ class User:
         self.location: Location = Location(location)       
 class Food:
     def __init__(self, name: str, weight: float, calories: int, description: str):
-        self.name: str = name
+        self.name: str = name.replace('w/', 'with')
         self.weight: float = weight
         self.calories: int = calories
         self.description: str = description
+        
+    def to_dict(self) -> dict:
+        return {"weight": self.weight, "calories": self.calories, "description": self.description}
 class Menu:
     def __init__(self, breakfast: dict, lunch: dict, dinner: dict):
         self.breakfast: list[Food] = [Food(name, **food) for name, food in breakfast.items()]
         self.lunch: list[Food] = [Food(name, **food) for name, food in lunch.items()]
         self.dinner: list[Food] = [Food(name, **food) for name, food in dinner.items()]
+        
+    def __init__(self):
+        self.breakfast: list[Food] = []
+        self.lunch: list[Food] = []
+        self.dinner: list[Food] = []
+
+    def to_dict(self) -> dict:
+        return {"breakfast": {food.name: food.to_dict() for food in self.breakfast}, 
+                "lunch": {food.name: food.to_dict() for food in self.lunch}, 
+                "dinner": {food.name: food.to_dict() for food in self.dinner}}
+
+    def add_food(self, meal_time: str, foods: list[Food]) -> None:
+        if meal_time == "breakfast":
+            self.breakfast.extend(foods)
+        elif meal_time == "lunch":
+            self.lunch.extend(foods)
+        elif meal_time == "dinner":
+            self.dinner.extend(foods)
+            
 class DiningHall:
     def __init__(self, name: str, location: dict[str, int], hours: list, menu: dict):
         self.name: str = name
@@ -66,14 +92,16 @@ class DiningHall:
         return start_time and any(start_time > start and start_time+duration < end \
                                 for start, end in zip(self.__hours[::2], self.__hours[1::2]))
     
-    def find_best_time(self, start_time: int, end_time: int, duration: int) -> int:
+    def find_best_time(self, start_time: int, end_time: int, duration: int) -> int | None:
         for start, end in zip(self.__hours[:-1], self.__hours[1:]):
-            # TODO
             if end_time > end and start_time < start:
                 return end_time - duration
             elif end_time > end and start_time > start:
                 if end_time - duration < start:
                     return end_time - duration
+            elif end_time < end and start_time < start:
+                if start_time + duration < end:
+                    return end - duration
                     
         return None
         
@@ -93,7 +121,9 @@ class FirebaseConnection:
         return real_time_db_app
 
 
-    def __add_dining_hall(self, name: str, location: [int, int], hours: list, menu: dict) -> None:
+    def __add_dining_hall(self, name: str, location: [int, int], hours: list, menu: dict | Menu) -> None:
+        if isinstance(menu, Menu):
+            menu = menu.to_dict()
         self.ref.child(f"dining_halls/{name}").set({"location": {"lat": location[0], "long": location[1]},
                                             "hours": hours,
                                             "menu": menu})
@@ -111,18 +141,46 @@ class FirebaseConnection:
     def __create_food(self, name: str, weight: float, calories: int, description: str) -> dict:
         return {"name": name, "weight": weight, "calories": calories, "description": description}
 
+    def __get_food_info(self, dining_hall: str, current_date: date, dining_time: str) -> list[Food]:
+        foods: list[Food] = []
+        website_url: str = f'https://eatatstate.msu.edu/menu/{dining_hall}/all/{str(current_date)}'
+        website_data: str = urllib.request.urlopen(website_url).read()
+        soup: BeautifulSoup = BeautifulSoup(website_data, "html.parser")
+
+        for tag in soup.find_all('li', attrs={'class': 'menu-item'}):
+            try: 
+                meal_name: str = tag.find('div', attrs={'class': 'meal-title '+dining_time}).text
+                description: str = tag.find('div', attrs={'class': 'allergens'}).text.replace('\n', '')
+                foods.append(Food(meal_name, 0.5, 0, description))
+            except:
+                continue
+        return foods
+
+    def __get_dining_hall_meals(self, dining_hall) -> Menu:
+        dining_hall_menu: Menu = Menu()
+        for meal_time in MEAL_TIMES:
+            foods: list[Food] = self.__get_food_info(DINING_HALLS_TO_HTML[dining_hall], date.today(), meal_time)
+            dining_hall_menu.add_food(meal_time, foods)
+        return dining_hall_menu
+
     def __init_dining_halls(self) -> None:
-        breakfast = [self.__create_food("Eggs", 0.2, 150, "Eggs"), self.__create_food("Bacon", 0.6, 200, "Bacon")]
-        lunch = [self.__create_food("Pizza", 0.5, 400, "Cheese"), self.__create_food("Cheese Burger", 0.4, 450, "Bread, Meat, Cheese")]
-        dinner = [self.__create_food("Chicken Sandwich", 0.6, 480, "Chicken, Bread"), self.__create_food("Tacos", 0.7, 600, "Steak, Tortilla, Cheese, Lettuce")]
-        case_menu = self.__create_menu(breakfast, lunch, dinner)
-        self.__add_dining_hall("Case", [42.7244946181507, -84.48848047008471], [700, 1500, 1630, 2100], case_menu)
+        # breakfast = [self.__create_food("Eggs", 0.2, 150, "Eggs"), self.__create_food("Bacon", 0.6, 200, "Bacon")]
+        # lunch = [self.__create_food("Pizza", 0.5, 400, "Cheese"), self.__create_food("Cheese Burger", 0.4, 450, "Bread, Meat, Cheese")]
+        # dinner = [self.__create_food("Chicken Sandwich", 0.6, 480, "Chicken, Bread"), self.__create_food("Tacos", 0.7, 600, "Steak, Tortilla, Cheese, Lettuce")]
+        # case_menu = self.__create_menu(breakfast, lunch, dinner)
+        # self.__add_dining_hall("Case", [42.7244946181507, -84.48848047008471], [700, 1500, 1630, 2100], case_menu)
     
-        breakfast = [self.__reate_food("Eggs", 0.2, 150, "Eggs"), self.__create_food("Bacon", 0.6, 200, "Bacon")]
-        lunch = [self.__create_food("Pizza", 0.5, 400, "Cheese"), self.__create_food("Cheese Burger", 0.4, 450, "Bread, Meat, Cheese")]
-        dinner = [self.__create_food("Chicken Sandwich", 0.6, 480, "Chicken, Bread"), self.__create_food("Tacos", 0.7, 600, "Steak, Tortilla, Cheese, Lettuce")]
-        akers_menu = self.__create_menu(breakfast, lunch, dinner)
-        self.__add_dining_hall("Akers", [42.724511319457186, -84.4648138643711], [700, 1500, 1630, 2100], akers_menu)
+        # breakfast = [self.__reate_food("Eggs", 0.2, 150, "Eggs"), self.__create_food("Bacon", 0.6, 200, "Bacon")]
+        # lunch = [self.__create_food("Pizza", 0.5, 400, "Cheese"), self.__create_food("Cheese Burger", 0.4, 450, "Bread, Meat, Cheese")]
+        # dinner = [self.__create_food("Chicken Sandwich", 0.6, 480, "Chicken, Bread"), self.__create_food("Tacos", 0.7, 600, "Steak, Tortilla, Cheese, Lettuce")]
+        # akers_menu = self.__create_menu(breakfast, lunch, dinner)
+        # self.__add_dining_hall("Akers", [42.724511319457186, -84.4648138643711], [700, 1500, 1630, 2100], akers_menu)
+        for dining_hall in DINING_HALLS_TO_HTML.keys():
+            # TODO get location and hours
+            self.__add_dining_hall(dining_hall, 
+                                   [42.724511319457186, -84.4648138643711], 
+                                   [700, 1500, 1630, 2100], 
+                                   self.__get_dining_hall_meals(dining_hall))
 
     def __create_class(self, location: [int, int], time: [int, int], days: list) -> dict:
         return {"location": {"lat": location[0], "long": location[1]}, 
