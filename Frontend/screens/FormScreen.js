efe2f401c44acaf6ad2cc0bccd6b7a02195e260a
Aidan Keighron <78337317+aidankeighron@users.noreply.github.com>
2024-01-28 10:43:13 -0500

    firebase

--- Frontend/screens/FormScreen.js
+++ Frontend/screens/FormScreen.js
@@ -2,11 +2,22 @@ import React, { useState } from 'react';
 import { View, Button, FlatList, Text } from 'react-native';
 import ModalComponent from './ModalComponent'; // Import your modal component
 
-
 import { collection, addDoc } from "firebase/firestore"; 
 
-import { getApps, initializeApp } from "firebase/app";
-import {app, db} from '../firebase'
+// PACKAGES
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, child, get } from "firebase/database";
+const firebaseConfig = {
+  apiKey: "AIzaSyCjYzQ8JfadVxf6llSikhjiEfpX-Lbg4iQ",
+  authDomain: "food-path-24322.firebaseapp.com",
+  databaseURL: "https://food-path-24322-default-rtdb.firebaseio.com",
+  projectId: "food-path-24322",
+  storageBucket: "food-path-24322.appspot.com",
+  messagingSenderId: "1084147699802",
+  appId: "1:1084147699802:web:f1e97a8fac6b203d16c863",
+  measurementId: "G-8QL9VFEXW9"
+};
+// PACKAGES
 
 
 
@@ -19,6 +30,19 @@ const YourScreen = ({navigation}) => {
     setModalVisible(false);
   };
 
+  // GET DATA
+  const app = initializeApp(firebaseConfig);
+  const dbRef = ref(getDatabase(app));
+  get(child(dbRef, `users/Aidan`)).then((snapshot) => {
+    if (snapshot.exists()) {
+      console.log(snapshot.val());
+    } else {
+      console.log("No data available");
+    }
+  }).catch((error) => {
+    console.error(error);
+  });
+  // GET DATA
   const handleRenderItem = ({item}) => {
     const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
