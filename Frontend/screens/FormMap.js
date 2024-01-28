dbcf4449e3b277bb480b96f42dcadc6309c2b285
Nathanael Lu <lunathanael@gmail.com>
2024-01-28 02:31:35 -0500

    form map updates

--- Frontend/screens/FormMap.js
+++ Frontend/screens/FormMap.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { Modal, View, ImageBackground, StyleSheet, Dimensions } from 'react-native';
 import { Block, Button, Text, theme } from 'galio-framework';
 
@@ -8,6 +8,7 @@ import materialTheme from '../constants/Theme';
 import locationData from '../constants/locationData';
 
 import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps';
+import * as Location from "expo-location";
 
 const DINING_HALLS_LOCATIONS = locationData.DINING_HALLS_LOCATIONS;
 
@@ -34,6 +35,75 @@ const mapCustomStyle = [
   { "featureType": "water", "elementType": "labels.text.fill", "stylers": [ { "color": "#515c6d" } ] }, 
   { "featureType": "water", "elementType": "labels.text.stroke", "stylers": [ { "color": "#17263c" } ] } 
 ];
+  
+const Waiting_Driver_Screen = ({handleSetLocation, mapCustomStyle, onMapPress}) => {
+    const [currentLocation, setCurrentLocation] = useState(null);
+    const [initialRegion, setInitialRegion] = useState(null);
+    const markers = [];
+
+    for (const hall in DINING_HALLS_LOCATIONS) {
+      markers.push({'key' : hall, 'coordinate': {'latitude': DINING_HALLS_LOCATIONS[hall][0], 'longitude': DINING_HALLS_LOCATIONS[hall][1]}});
+    }
+  
+    useEffect(() => {
+
+      const getLocation = async () => {
+        let { status } = await Location.requestForegroundPermissionsAsync();
+        let location = {'coords': {'latitude': 42.7229439975, 'longitude': -84.4792321495}};
+        if (status !== "granted") {
+          console.log("Permission to access location was denied");
+        }
+        else {
+            location = await Location.getCurrentPositionAsync({});
+        }
+  
+        setCurrentLocation(location.coords);
+  
+        setInitialRegion({
+          latitude: location.coords.latitude,
+          longitude: location.coords.longitude,
+          latitudeDelta: 0.0422,
+          longitudeDelta: 0.0221,
+        });
+      };
+  
+      getLocation();
+    }, []);
+  
+    return (
+      <View style={styles.container}>
+        {initialRegion && (
+            
+          <MapView 
+          style={styles.map} 
+          initialRegion={initialRegion}
+          customMapStyle={mapCustomStyle}
+          onPress={onMapPress}>
+            {currentLocation && (
+              <Marker
+                coordinate={{
+                  latitude: currentLocation.latitude,
+                  longitude: currentLocation.longitude,
+                }}
+                title="Your Location"
+              />
+            )}
+            {markers.map((marker) => (
+            <Marker
+              title={marker.key}
+              key={marker.key}
+              coordinate={marker.coordinate}
+              icon={require('../assets/icons/foodIcon.png')}
+              onPress={() => {
+                handleSetLocation(marker.coordinate)
+              }}>
+            </Marker>
+          ))}
+          </MapView>
+        )}
+      </View>
+    );
+};
   
 
 export default FormMap = ({isVisible, onAddLocation, onClose, setLocation}) => {
@@ -64,12 +134,17 @@ export default FormMap = ({isVisible, onAddLocation, onClose, setLocation}) => {
       <Block flex style={styles.container}>
         
         <Block flex center>
-          <MapView
+            <Waiting_Driver_Screen
+            handleSetLocation={handleSetLocation} 
+            mapCustomStyle={mapCustomStyle} 
+            onMapPress={this.onMapPress}/>
+          {/* <MapView
             provider={PROVIDER_GOOGLE}
             style={styles.map}
             initialRegion={this.state.region}
             customMapStyle={mapCustomStyle}
-            onPress={this.onMapPress}>
+            onPress={this.onMapPress}
+            showsUserLocation={true}>
               
             {this.state.markers.map((marker) => (
               <Marker
@@ -82,7 +157,7 @@ export default FormMap = ({isVisible, onAddLocation, onClose, setLocation}) => {
                 }}>
               </Marker>
             ))}
-          </MapView>
+          </MapView> */}
         </Block>
         <Block flex space="between" style={styles.padded}>
           <Block flex space="around" style={{ zIndex: 1 }}>
